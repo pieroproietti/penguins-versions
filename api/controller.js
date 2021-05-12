d5af55330e7bd101ef0c48a1f6e5c3cef6ecb2c1
Piero Proietti <piero.proietti@postecert.it>
2021-05-12 16:39:49 +0200

    riaddedd krill

--- api/controller.js
+++ api/controller.js
@@ -4,6 +4,7 @@ var properties = require('../package.json')
 
 var Datastore = require('nedb')
 var versions = new Datastore({ filename: 'versions.db', autoload: true })
+var krillVersions = new Datastore({ filename: 'krillVersions.db', autoload: true })
 
 var controllers = {
 
@@ -72,6 +73,67 @@ var controllers = {
             res.json(docs);
         })
     },
+
+
+    /**
+     * KRILL
+     * 
+     */
+    /**
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+     krillAdd: function (req, res) {
+        let version = req.params.version
+        let arch = req.params.arch
+        let changelog = req.params.changelog
+        const record = { version, arch, changelog }
+
+        krillVersions.insert(record, function (err, doc) {
+            if (err)
+                res.send(err)
+            res.json(record)
+        });
+    },
+
+    /**
+     * 
+     * @param {*} req 
+     * @param {*} res 
+     */
+    versions: function (req, res) {
+        let version = req.params.version
+        let arch = req.params.arch
+
+        if (version === undefined) {
+            version = 'all'
+        }
+
+        if (arch === undefined) {
+            arch = 'all'
+        }
+
+        let criteria = {}
+        if (version === 'all' && arch === 'all') {
+            criteria = {}
+        } else if (version !== 'all' && arch === 'all') {
+            criteria = { version: version }
+        } else if (version !== 'all' && arch !== 'all') {
+            criteria = { version: version, arch: arch }
+        } else if (version === 'all' && arch !== 'all') {
+            criteria = { arch: arch }
+        }
+
+        krillVersions.find(criteria, function (err, docs) {
+            if (err)
+                res.send(err)
+            res.json(docs);
+        })
+    },
+
+
+
 }
 
 module.exports = controllers
