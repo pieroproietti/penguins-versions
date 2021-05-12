57b904227ba943e768a9063a974b2322d6a6fad0
Piero Proietti <piero.proietti@postecert.it>
2021-05-12 15:41:34 +0200

    tests

--- api/routes.js
+++ api/routes.js
@@ -6,28 +6,28 @@ module.exports = function (app) {
     app.route('/about')
         .get(controller.about)
 
-        app.route('/versions/')
+    app.route('/versions/')
         .get(controller.versions)
 
-        app.route('/versions/:version/:arch')
+    app.route('/versions/:version/:arch')
         .get(controller.versions)
 
-        app.route('/versions/:version/')
-       .get(controller.versions)
+    app.route('/versions/:version/')
+        .get(controller.versions)
 
     app.route('/add/:version/:arch/:changelog')
-        .get(krillController.add)
+        .get(controller.add)
 
-        app.route('/krill/versions/')
+    app.route('/krill/versions/')
         .get(krillController.versions)
 
-        app.route('/krill/versions/:version/:arch')
+    app.route('/krill/versions/:version/:arch')
         .get(krillController.versions)
 
-        app.route('/krill/versions/:version/')
-       .get(krillController.versions)
+    app.route('/krill/versions/:version/')
+        .get(krillController.versions)
 
     app.route('/krill/add/:version/:arch/:changelog')
         .get(krillController.add)
 
-    }
+}
