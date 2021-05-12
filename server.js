710d65a2c3a0ce83edb30be8ecf40c21772bae83
Piero Proietti <piero.proietti@postecert.it>
2021-05-12 16:34:48 +0200

    rimesso pure server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 3000;
 
-const routes = require('./api/routes').default;
+const routes = require('./api/routes');
 routes(app);
 app.listen(port, function() {
    console.log('Server started on port: ' + port);
