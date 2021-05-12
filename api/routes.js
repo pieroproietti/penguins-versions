fad6a2fd06796fbf098876e99700768ccd89f09b
Piero Proietti <piero.proietti@postecert.it>
2021-05-12 16:46:25 +0200

    riaggiunte routes krill

--- api/routes.js
+++ api/routes.js
@@ -6,15 +6,33 @@ module.exports = function (app) {
     app.route('/about')
         .get(controller.about)
 
-        app.route('/versions/')
+    /**
+     * eggs
+     */
+    app.route('/versions/')
         .get(controller.versions)
 
-        app.route('/versions/:version/:arch')
+    app.route('/versions/:version/:arch')
         .get(controller.versions)
 
-        app.route('/versions/:version/')
-       .get(controller.versions)
+    app.route('/versions/:version/')
+        .get(controller.versions)
 
     app.route('/add/:version/:arch/:changelog')
+        .get(controller.add)
+
+    /** 
+     * krill
+     */
+    app.route('/krill/versions/')
+        .get(controller.versions)
+
+    app.route('/krill/versions/:version/:arch')
+        .get(controller.versions)
+
+    app.route('/krill/versions/:version/')
+        .get(controller.versions)
+
+    app.route('/krill/add/:version/:arch/:changelog')
         .get(controller.add)
 }
