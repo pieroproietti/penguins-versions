2072040814d80e4d01f8a3672e33c6ed3714796f
Piero Proietti <piero.proietti@postecert.it>
2021-05-12 16:19:04 +0200

    back

--- api/routes.js
+++ api/routes.js
@@ -1,7 +1,6 @@
 'use strict';
 
-import { about, versions, add } from './controller';
-import { krillVersions, krillAdd } from './krill_controller';
+import { about, versions, add} from './controller';
 
 export default function (app) {
     app.route('/about')
@@ -22,18 +21,4 @@ export default function (app) {
     app.route('/add/:version/:arch/:changelog')
         .get(add)
 
-    /**
-     * krill
-     */
-    app.route('/krill/versions/')
-        .get(krillVersions)
-
-    app.route('/krill/versions/:version/:arch')
-        .get(krillVersions)
-
-    app.route('/krill/versions/:version/')
-        .get(krillVersions)
-
-    app.route('/krill/add/:version/:arch/:changelog')
-        .get(krillAdd)
 }
